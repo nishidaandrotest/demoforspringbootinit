7e4dc28732b25c7d8302ad9b600bae03143b75d0
nishidaandrotest <nishidaandrotest@gmail.com>
2016-10-08 14:02:58 +0900

    init

---
+++ src/main/java/jp/co/test/DemoforspringbootApplication.java
@@ -0,0 +1,12 @@
+package jp.co.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforspringbootApplication.class, args);
+	}
+}
